e662bb0fbe05b4dbcfb9b2a81c175270ecdf934b
Firmino Neto <firminoneto11@hotmail.com>
2022-06-04 22:05:55 -0300

    Final implementation of the views Implemented the actions for retrieving, updating and deleting recipes and also tests for all of those actions

--- apps/core/tests/test_views.py
+++ apps/core/tests/test_views.py
@@ -22,6 +22,10 @@ class TestRecipesView(TestCase):
         self.test_recipe.delete()
 
     def assert_dictionaries(self, data: dict):
+        """
+        This method is a helper to assert that the data is in a dictionary format, has a n amount of keys and if those keys are the
+        same as the pre defined keys
+        """
 
         # Checking the type of the response's object
         self.assertTrue(isinstance(data, dict), "The response data should be in a dictionary format")
@@ -37,12 +41,58 @@ class TestRecipesView(TestCase):
         for key in data.keys():
             self.assertTrue(key in self.response_format_data, f"The key '{key}' is not supposed to be in the object")
 
-    def test_list_recipes(self):
-        response = self.client.get(self.recipes_url)
+    def test_create_recipe(self):
+        """
+        This method tests if the 'create_recipe' method of the RecipesViewSet is able to create a recipe with the given data
+        """
+        response = self.client.post(self.recipes_url, data=self.mock_data, content_type="application/json")
         response_data = response.json()
+
+        self.assertEqual(response.status_code, 201, "Status code should be 201")
+        self.assert_dictionaries(data=response_data)
+
+        recipe = None
+        try:
+            recipe = Recipe.objects.get(id=response_data["id"])
+        except:
+            pass
+
+        # The Recipe item should exists in the database with the id returned
+        self.assertTrue(isinstance(recipe, Recipe), "The recipe should exists in the database")
+
+        # Checking if the values matches with what was informed in the request
+        keys = ["title", "text", "key"]
+        for key in keys:
+            db_value = getattr(recipe, key)
+            self.assertEqual(
+                db_value,
+                self.mock_data[key],
+                f"The value for {key} should be '{self.mock_data[key]}' not {db_value}",
+            )
+
+        # Checking if the view doesn't allow the creation of empty recipes
+        response = self.client.post(self.recipes_url, data=dict(), content_type="application/json")
+        self.assertEqual(response.status_code, 400, "Should not be able to create an empty recipe")
+
+        # Checking if the view doesn't allow the creation of recipes with missing data
+        for key in keys:
+            post_data = {key: self.mock_data[key]}
+            response = self.client.post(self.recipes_url, data=post_data, content_type="application/json")
+            self.assertEqual(
+                response.status_code, 400, f"Should not be able to create a recipe with only '{key}' data"
+            )
+
+    def test_list_recipes(self):
+        """
+        This method tests if the 'list_recipes' method of the view RecipesViewSet is able to return a list of Recipes
+        """
+
+        response = self.client.get(self.recipes_url)
 
         # Checking for status code
         self.assertEqual(response.status_code, 200, "Status code should be 200")
+
+        response_data = response.json()
 
         # Checking for type of the response
         self.assertTrue(isinstance(response_data, list), "Response object should be a list")
@@ -51,48 +101,114 @@ class TestRecipesView(TestCase):
         self.assertGreaterEqual(len(response_data), 1, "Length of the response list should be greater or equal to 1")
 
         first_element = response_data[0]
-
         self.assert_dictionaries(data=first_element)
 
-        # Checking if the data matches what was informed in the setUp function
-        self.assertEqual(first_element["title"], "Quick cake recipe", "The title should be 'Quick cake recipe'")
+        # Checking if the data matches with what was informed in the setUp function
         self.assertEqual(
-            first_element["text"], "1) Buy a cake. The end", "The text should be '1) Buy a cake. The end'"
+            first_element["title"], self.test_recipe.title, f"The title should be '{self.test_recipe.title}'"
         )
+        self.assertEqual(first_element["text"], self.test_recipe.text, f"The text should be '{self.test_recipe.text}'")
 
-    def test_create_recipe(self):
-        response = self.client.post(self.recipes_url, data=self.mock_data, content_type="application/json")
+    def test_get_recipe(self):
+        """
+        This method tests if the 'get_recipe' method of the RecipesViewSet is able to retrieve a single Recipe from the database with
+        a given id in route param
+        """
+        manage_recipes_url = reverse("core:manage-recipe", kwargs={"recipe_id": self.test_recipe.pk})
+        response = self.client.get(manage_recipes_url)
+
+        # Checking for status code
+        self.assertEqual(response.status_code, 200, "Status code should be 200")
+
         response_data = response.json()
-
-        self.assertEqual(response.status_code, 201, "Status code should be 201")
-
         self.assert_dictionaries(data=response_data)
 
-        Recipe.objects.get(id=response_data["id"]).delete()
+        # Checking if the data matches with what was informed in the setUp function
+        self.assertEqual(
+            response_data["title"], self.test_recipe.title, f"The title should be '{self.test_recipe.title}'"
+        )
+        self.assertEqual(response_data["text"], self.test_recipe.text, f"The text should be '{self.test_recipe.text}'")
 
-        # Checking if the values matches with what was informed in the request
+        nonexistent_id = Recipe.objects.last().pk + 1
+        url = reverse("core:manage-recipe", kwargs={"recipe_id": nonexistent_id})
+        response = self.client.get(url)
+
+        # Checking if the view isn't trying to fetch something that doesn't exists
+        self.assertEqual(response.status_code, 404, "Should not be able to fetch a recipe that doesn't exists")
+
+    def test_update_recipe(self):
+        """
+        This method tests if the method 'update_recipe' of the RecipesViewSet is able to update a single recipe from the database.
+        Also checks if someone is able to update a recipe without informing the recipe's key or if the key is wrong.
+        """
+        recipe: Recipe = Recipe.objects.create(
+            title=self.mock_data["title"], text=self.mock_data["text"], key=self.mock_data["key"]
+        )
+
+        put_data = {"title": "A new recipe", "text": "How cool is this recipe?", "key": self.mock_data["key"]}
+
+        manage_recipes_url = reverse("core:manage-recipe", kwargs={"recipe_id": recipe.pk})
+        response = self.client.put(manage_recipes_url, data=put_data, content_type="application/json")
+
+        # Checking for the status code
+        self.assertEqual(response.status_code, 200, "Status code should be 200")
+
+        # Checking if the values in the database corresponds to what was informed in the request
+        recipe = Recipe.objects.get(id=recipe.pk)
         keys = ["title", "text"]
         for key in keys:
+            db_value = getattr(recipe, key)
             self.assertEqual(
-                response_data[key],
-                self.mock_data[key],
-                f"The response value for {key} should be '{self.mock_data[key]}' not {response_data[key]}",
+                db_value, put_data[key], f"The value of '{key}' should be '{put_data['key']}' not '{db_value}'"
             )
 
-        response = self.client.post(self.recipes_url, data=dict(), content_type="application/json")
-        self.assertEqual(response.status_code, 400, "Should not be able to create an empty recipe")
+        new_data = {"title": "A new title", "text": "A new text"}
+        response = self.client.put(manage_recipes_url, data=new_data, content_type="application/json")
 
-        response = self.client.post(
-            self.recipes_url, data={"title": self.mock_data["title"]}, content_type="application/json"
-        )
-        self.assertEqual(response.status_code, 400, "Should not be able to create a recipe without text and key")
+        # Checking if the view isn't updating a recipe without informing it's key
+        self.assertEqual(response.status_code, 400, "Should not be able to update a recipe without informing the key")
 
-        response = self.client.post(
-            self.recipes_url, data={"text": self.mock_data["text"]}, content_type="application/json"
-        )
-        self.assertEqual(response.status_code, 400, "Should not be able to create a recipe without title and key")
+        new_data["key"] = "a different key"
+        response = self.client.put(manage_recipes_url, data=new_data, content_type="application/json")
 
-        response = self.client.post(
-            self.recipes_url, data={"key": self.mock_data["key"]}, content_type="application/json"
+        # Checking if the view isn't updating a recipe without the key being equals to the recipe's key
+        self.assertEqual(response.status_code, 400, "Should not be able to update a recipe with the wrong key")
+
+        nonexistent_id = Recipe.objects.last().pk + 1
+        url = reverse("core:manage-recipe", kwargs={"recipe_id": nonexistent_id})
+        response = self.client.put(url, data=new_data, content_type="application/json")
+
+        # Checking if the view isn't trying to update something that doesn't exists
+        self.assertEqual(response.status_code, 404, "Should not be able to update a recipe that doesn't exists")
+
+    def test_delete_recipe(self):
+        """
+        This method tests if the 'delete_recipe' method of RecipesViewSet is able to delete a single Recipe from the database with a
+        given id in route param
+        """
+        recipe: Recipe = Recipe.objects.create(
+            title=self.mock_data["title"], text=self.mock_data["text"], key=self.mock_data["key"]
         )
-        self.assertEqual(response.status_code, 400, "Should not be able to create a recipe without title and text")
+
+        manage_recipes_url = reverse("core:manage-recipe", kwargs={"recipe_id": recipe.pk})
+        response = self.client.delete(manage_recipes_url)
+
+        # Checking for the status code
+        self.assertEqual(response.status_code, 204, "Status code should be 204")
+
+        old_pk = recipe.pk
+        recipe = None
+        try:
+            recipe = Recipe.objects.get(id=old_pk)
+        except:
+            pass
+
+        # Checking if the Recipe was actually deleted
+        self.assertTrue(recipe is None, f"The recipe of id {old_pk} should've been deleted")
+
+        nonexistent_id = Recipe.objects.last().pk + 1
+        url = reverse("core:manage-recipe", kwargs={"recipe_id": nonexistent_id})
+        response = self.client.delete(url)
+
+        # Checking if the view isn't trying to delete something that doesn't exists
+        self.assertEqual(response.status_code, 404, "Should not be able to delete a recipe that doesn't exists")
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.viewsets import GenericViewSet as Gen
@@ -11,20 +12,45 @@ class RecipesViewSet(Gen):
     serializer_class = RecipeSerializer
 
     def create_recipe(self, req: Request):
+        """This view creates a new Recipe into the database with the data given in req.data"""
         serialized_object: RecipeSerializer = self.get_serializer(data=req.data)
         serialized_object.is_valid(raise_exception=True)
         serialized_object.save()
         return Response(data=serialized_object.data, status=201)
 
-    def list_recipes(self, req: Request):
+    def list_recipes(self, _req: Request):
+        """This view return all the recipes in the database in a list"""
         serialized_data = self.get_serializer(instance=self.get_queryset(), many=True).data
-        return Response(data=serialized_data, status=200)
+        return Response(data=serialized_data)
 
-    def get_recipe(self, req: Request, recipe_id: int):
-        pass
+    def get_recipe(self, _req: Request, recipe_id: int):
+        """This view returns a single Recipe from the database based on the id informed on the route param"""
+        recipe = get_object_or_404(Recipe, id=recipe_id)
+        serialized_recipe_data = self.get_serializer(instance=recipe).data
+        return Response(data=serialized_recipe_data)
 
     def update_recipe(self, req: Request, recipe_id: int):
-        pass
+        """
+        This view updates a single Recipe based on the id informed on the route param. In the req.data the 'key' prop must be set and it
+        should be the same as the 'key' property of the Recipe that is going to be updated. This way we make sure that only the person
+        who created the Recipe can edit it
+        """
+        recipe: Recipe = get_object_or_404(Recipe, id=recipe_id)
 
-    def delete_recipe(self, req: Request, recipe_id: int):
-        pass
+        if "key" not in req.data.keys():
+            return Response(data={"detail": "The Recipe's key must be set"}, status=400)
+        if req.data["key"] != recipe.key:
+            return Response(data={"detail": "The Recipe's key is invalid"}, status=400)
+
+        serialized_recipe: RecipeSerializer = self.get_serializer(instance=recipe, data=req.data, partial=True)
+        serialized_recipe.is_valid(raise_exception=True)
+        serialized_recipe.save()
+        return Response(data=serialized_recipe.data)
+
+    def delete_recipe(self, _req: Request, recipe_id: int):
+        """
+        This view deletes a single Recipe from the database based on the id informed on the route param.
+        """
+        recipe: Recipe = get_object_or_404(Recipe, id=recipe_id)
+        recipe.delete()
+        return Response(status=204)
